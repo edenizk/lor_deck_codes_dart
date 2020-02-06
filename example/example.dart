839c71889fff7a12fbcde44b6dd58bf017ee5760
edenizk <dkokyanus@gmail.com>
2020-02-07 00:27:18 +0100

    simple use of the package

---
+++ example/example.dart
@@ -0,0 +1,12 @@
+import 'package:lor_deck_codes_dart/CardCodeAndCount.dart';
+import 'package:lor_deck_codes_dart/LoRDeckEncoder.dart';
+
+void main() {
+  List<CardCodeAndCount> deck = LoRDeckEncoder.GetDeckFromCode(
+      'CEAQQAIAAECAMFBCEQTTMAQCAEBASGQFAEAAGCYSDUXQCAQBAADQY');
+  for (var card in deck) {
+    print(card.Count.toString() + ':' + card.CardCode);
+  }
+  String code = LoRDeckEncoder.GetCodeFromDeck(deck);
+  print(code);
+}
